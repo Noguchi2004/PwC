eb369e71204d29cc9c4b10b9f3196307f7dd91af
pedro <pedrohcnoguti@gmail.com>
2024-02-14 19:46:08 -0300

    Último commit 14/02

--- DesafioPwC/Endereco.cs
+++ DesafioPwC/Endereco.cs
@@ -9,7 +9,6 @@ public class Endereco
 
 	}
 
-	//"     rua dos duendes, 8888
 	public String Converte(String endereco)
 	{
 		String Numero = String.Empty;
@@ -17,7 +16,7 @@ public class Endereco
 		int numTeste;
 
 		List<String> stringDividida = endereco.Split(" ", StringSplitOptions.None).ToList();
-		
+
 		if (int.TryParse(stringDividida[0], out numTeste))
 		{
 			Numero = stringDividida[0];
@@ -27,16 +26,17 @@ public class Endereco
 
 		}
 		else if (Regex.IsMatch(stringDividida[stringDividida.Count - 1], @"\d$"))
-		{R
-            Numero = stringDividida[stringDividida.Count-1];
+		{
+			Numero = stringDividida[stringDividida.Count - 1];
 			stringDividida.RemoveAt(stringDividida.Count - 1);
 			Rua = string.Join(" ", stringDividida);
-        }
-		else
-		{
-			Numero = "caso2";
-			Rua = "caso2";
-
+		}
+		else {
+			for (int i = stringDividida.Count - 1; i >= 0; i--)
+			{
+				Numero = stringDividida[]
+	
+			}
 		}
 		return $"{{\"{Rua}\"}},\"{{\"{Numero}\"}}";
 
--- DesafioPwC/Program.cs
+++ DesafioPwC/Program.cs
@@ -4,7 +4,10 @@ class Program{
     public static void Main(string[] args)
     {
         var entrada = string.Join(" ", args);
-
+        entrada ="Rua dos milagres 12";
+        //entrada ="13, Rua dos milagres";
+        //entrada ="Rua dos milagres, 12";
+        
         var endereco = new Endereco();
         var retorno = endereco.Converte(entrada);
         Console.WriteLine(retorno);
