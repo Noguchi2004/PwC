9d41fb142f971baeadff04040d594ef7cc911fc1
pedro <pedrohcnoguti@gmail.com>
2024-02-18 17:24:22 -0300

    Código com os casos de teste.

--- DesafioPwC/Program.cs
+++ DesafioPwC/Program.cs
@@ -4,15 +4,33 @@ class Program{
     public static void Main(string[] args)
     {
      
-        if (args.Length == 0)
-        {
-            Console.WriteLine("Informe um endereço!");
-            return;
+        if (args.Length != 0)
+        { 
+            var entrada = string.Join(" ", args);
+            var endereco = new Endereco();
+            var retorno = endereco.Converte(entrada);
+            Console.WriteLine(retorno);
         }
-        var entrada = string.Join(" ", args);
+
+        Converte("Miritiba 339");
+        Converte("Babaçu 500");
+        Converte("Cambuí 804B");
+        Converte("Rio Branco 23");
+        Converte("Quirino dos Santos 23 b");
+        Converte("4, Rue de la Republique");
+        Converte("100 Broadway Av");
+        Converte("Calle Sagasta, 26");
+        Converte("Calle 44 No 1991");
+
+    }
+
+
+
+    private static void Converte(string texto)
+    {
         var endereco = new Endereco();
-        var retorno = endereco.Converte(entrada);
-        Console.WriteLine(retorno);
+        var retorno = endereco.Converte(texto);
+        Console.WriteLine(texto +" > "+ retorno);
        
     }
 }
